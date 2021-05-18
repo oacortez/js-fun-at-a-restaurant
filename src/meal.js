ffa9137270cc60d000a47bf0ff10dd55bd6dcf4a
Oscar Cortez <oacortez98@gmail.com>
2021-05-17 18:56:35 -0600

    Completed Half of meal project

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,31 @@
-function nameMenuItem() {
-
+function nameMenuItem(name){
+return `Delicious ${name}`;
 }
 
+function createMenuItem (name,price,type){
+  var menuItem = {
+    name : name,
+    price : price,
+    type : type
+  }
+  return menuItem;
+}
+
+function addIngredients (name,ingredients){
+  if (ingredients.includes(name)){
+    return ingredients;
+  } else {
+    ingredients.push(name);
+  }
+
+  return ingredients;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
